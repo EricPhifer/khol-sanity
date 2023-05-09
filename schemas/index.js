17e7528ed26d267eb9c22c5684e5fdfcdd685e39
Eric Phifer <ericoncode@gmail.com>
2023-05-09 11:53:43 -0600

    add contact and volunteer, update index to export default

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,37 @@
-export const schemaTypes = []
+// Navigation
+import navigation from "./navigation";
+// Sections
+import hero from "./hero";
+import about from "./about";
+import donate from "./donate";
+import partners from "./partners";
+import pastevents from "./pastevents";
+// Forms
+import contact from "./contact";
+import volunteer from "./volunteer";
+import testimonials from "./testimonials";
+// Privacy and Terms
+import privacypolicy from "./privacypolicy";
+import termsconditions from "./termsconditions";
+// Components
+import PageLinks from "../components/PageLinks"
+
+export default [
+    // Navigation
+    navigation,
+    // Sections
+    hero,
+    about,
+    donate,
+    partners,
+    pastevents,
+    // Forms
+    contact,
+    volunteer,
+    testimonials,
+    // Privacy and Terms
+    privacypolicy,
+    termsconditions,
+    // Components
+    PageLinks
+]
